d0b29731e4f4da873d6cdcf4727c90e52c15888c
Marcus Behel <marcus.behel@mindspring.com>
2022-09-11 11:39:06 -0400

    More matrix operations

--- math/c_motor_math/include/matrix.h
+++ math/c_motor_math/include/matrix.h
@@ -4,6 +4,15 @@
 
 
 ////////////////////////////////////////////////////////////////////////////////
+/// Macros
+////////////////////////////////////////////////////////////////////////////////
+
+#define MAT_ERR_NONE                0   // No error
+#define MAT_ERR_SIZE                -1  // Invalid size(s)
+#define MAT_ERR_INDX                -2  // Invalid index
+
+
+////////////////////////////////////////////////////////////////////////////////
 /// Typedefs
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -23,8 +32,9 @@ typedef struct{
  * @param m Pointer to the matrix to initialize
  * @param rows Number of rows in matrix
  * @param cols Number of columns in matrix
+ * @return Error code
  */
-void matrix_init(matrix *m, size_t rows, size_t cols);
+int matrix_init(matrix *m, size_t rows, size_t cols);
 
 /**
  * Free float matrix
@@ -44,46 +54,119 @@ void matrix_print(matrix *m);
  * @param row Row to set item in
  * @param col Column to set item in
  * @param item Value to set
+ * @return Error code
  */
-void matrix_set_item(matrix *m, size_t row, size_t col, float item);
+int matrix_set_item(matrix *m, size_t row, size_t col, float item);
 
 /**
  * Set a row in a matrix
  * @param m Pointer to the matrix to set a row in
  * @param row Row to set
  * @param data Pointer to data to insert into row (must be same size as the row)
+ * @return Error code
  */
-void matrix_set_row(matrix *m, size_t row, float *data);
+int matrix_set_row(matrix *m, size_t row, float *data);
 
 /**
  * Set a column in a matrix
  * @param m Pointer to the matrix to set a column in
  * @param col Column to set
  * @param data Pointer to data to insert into column (must be same size as the column)
+ * @return Error code
  */
-void matrix_set_col(matrix *m, size_t col, float *data);
+int matrix_set_col(matrix *m, size_t col, float *data);
 
 /**
  * Get an item in a matrix
  * @param m Pointer to the matrix to set an item in
  * @param row Row to set item in
  * @param col Column to set item in
- * @return Value of the item at the given position
+ * @param item Pointer to float to store item in
+ * @return Error code
  */
-float matrix_get_item(matrix *m, size_t row, size_t col);
+int matrix_get_item(matrix *m, size_t row, size_t col, float *item);
 
 /**
  * Get a row in a matrix
  * @param m Pointer to the matrix to get a row in
  * @param row Row to get
  * @param data Pointer to location to read matrix row into (must be same size as a row)
+ * @return Error code
  */
-void matrix_get_row(matrix *m, size_t row, float *data);
+int matrix_get_row(matrix *m, size_t row, float *data);
 
 /**
  * Get a column in a matrix
  * @param m Pointer to the matrix to get a column in
  * @param col Column to get
  * @param data Pointer to location to read matrix column into (must be same size as a column)
+ * @return Error code
  */
-void matrix_get_col(matrix *m, size_t col, float *data);
+int matrix_get_col(matrix *m, size_t col, float *data);
+
+/**
+ * Add two matrices and store the result in a third matrix
+ * dest = src1 + src2
+ * All three matrices must be the same size
+ * @param dest Pointer to destination matrix
+ * @param src1 Pointer to input operand 1
+ * @param src2 Pointer to input operand 2
+ * @return Error code
+ */
+int matrix_add(matrix *dest, matrix *src1, matrix *src2);
+
+/**
+ * Add two matrices and store the result in a third matrix
+ * dest = src1 + src2
+ * All three matrices must be the same size
+ * @param dest Pointer to destination matrix
+ * @param src1 Pointer to input operand 1
+ * @param src2 Pointer to input operand 2
+ * @return Error code
+ */
+int matrix_add(matrix *dest, matrix *src1, matrix *src2);
+
+/**
+ * Subtract two matrices and store the result in a third matrix
+ * dest = src1 - src2
+ * All three matrices must be the same size
+ * @param dest Pointer to destination matrix
+ * @param src1 Pointer to input operand 1
+ * @param src2 Pointer to input operand 2
+ * @return Error code
+ */
+int matrix_sub(matrix *dest, matrix *src1, matrix *src2);
+
+/**
+ * Multiply corresponding elements of the two source matrices and store the
+ * result in the destination matrix.
+ * @param dest Pointer to destination matrix
+ * @param src1 Pointer to input operand 1
+ * @param src2 Pointer to input operand 2
+ * @return Error code
+ */
+int matrix_ew_mul(matrix *dest, matrix *src1, matrix *src2);
+
+/**
+ * Divide corresponding elements of the two source matrices and store the
+ * result in the destination matrix.
+ * @param dest Pointer to destination matrix
+ * @param src1 Pointer to input operand 1
+ * @param src2 Pointer to input operand 2
+ * @return Error code
+ */
+int matrix_ew_div(matrix *dest, matrix *src1, matrix *src2);
+
+// TODO: Elementwise multiply and divide
+
+// TODO: Transpose
+
+// TODO: Matrix multiply
+
+// TODO: Inverse
+
+// TODO: Vector dot
+
+// TODO: Vector cross
+
+// TODO: Scalar multiply and divide
--- math/c_motor_math/src/matrix.c
+++ math/c_motor_math/src/matrix.c
@@ -15,10 +15,13 @@
 /// Functions
 ////////////////////////////////////////////////////////////////////////////////
 
-void matrix_init(matrix *m, size_t rows, size_t cols){
+int matrix_init(matrix *m, size_t rows, size_t cols){
+    if(rows == 0 || cols == 0)
+        return MAT_ERR_SIZE;
     m->data = calloc(rows * cols, sizeof(float));
     m->rows = rows;
     m->cols = cols;
+    return MAT_ERR_NONE;
 }
 
 void matrix_free(matrix *m){
@@ -34,34 +37,97 @@ void matrix_print(matrix *m){
     }
 }
 
-void matrix_set_item(matrix *m, size_t row, size_t col, float item){
+int matrix_set_item(matrix *m, size_t row, size_t col, float item){
+    if(row >= m->rows || col >= m->cols)
+        return MAT_ERR_INDX;
     m->data[MAT_IDX(m, row, col)] = item;
+    return MAT_ERR_NONE;
 }
 
-void matrix_set_row(matrix *m, size_t row, float *data){
+int matrix_set_row(matrix *m, size_t row, float *data){
+    if(row >= m->rows)
+        return MAT_ERR_INDX;
     for(size_t col = 0; col < m->cols; ++col){
         m->data[MAT_IDX(m, row, col)] = data[col];
     }
+    return MAT_ERR_NONE;
 }
 
-void matrix_set_col(matrix *m, size_t col, float *data){
+int matrix_set_col(matrix *m, size_t col, float *data){
+    if(col >= m->cols)
+        return MAT_ERR_INDX;
     for(size_t row = 0; row < m->rows; ++row){
         m->data[MAT_IDX(m, row, col)] = data[row];
     }
+    return MAT_ERR_NONE;
 }
 
-float matrix_get_item(matrix *m, size_t row, size_t col){
-    return m->data[MAT_IDX(m, row, col)];
+int matrix_get_item(matrix *m, size_t row, size_t col, float *item){
+    if(row >= m->rows || col >= m->cols)
+        return MAT_ERR_INDX;
+    *item = m->data[MAT_IDX(m, row, col)];
+    return MAT_ERR_NONE;
 }
 
-void matrix_get_row(matrix *m, size_t row, float *data){
+int matrix_get_row(matrix *m, size_t row, float *data){
+    if(row >= m->rows)
+        return MAT_ERR_INDX;
     for(size_t col = 0; col < m->cols; ++col){
         data[col] = m->data[MAT_IDX(m, row, col)];
     }
+    return MAT_ERR_NONE;
 }
 
-void matrix_get_col(matrix *m, size_t col, float *data){
+int matrix_get_col(matrix *m, size_t col, float *data){
+    if(col >= m->cols)
+        return MAT_ERR_INDX;
     for(size_t row = 0; row < m->rows; ++row){
         data[row] = m->data[MAT_IDX(m, row, col)];
     }
+    return MAT_ERR_NONE;
 }
+
+int matrix_add(matrix *dest, matrix *src1, matrix *src2){
+    if(dest->rows != src1->rows || dest->rows != src2->cols)
+        return MAT_ERR_SIZE;
+    for(size_t row = 0; row < dest->rows; ++row){
+        for(size_t col = 0; col < dest->cols; ++col){
+            dest->data[MAT_IDX(dest, row, col)] = src1->data[MAT_IDX(src1, row, col)] + src1->data[MAT_IDX(src2, row, col)];
+        }
+    }
+    return MAT_ERR_NONE;
+}
+
+int matrix_sub(matrix *dest, matrix *src1, matrix *src2){
+    if(dest->rows != src1->rows || dest->rows != src2->cols)
+        return MAT_ERR_SIZE;
+    for(size_t row = 0; row < dest->rows; ++row){
+        for(size_t col = 0; col < dest->cols; ++col){
+            dest->data[MAT_IDX(dest, row, col)] = src1->data[MAT_IDX(src1, row, col)] - src1->data[MAT_IDX(src2, row, col)];
+        }
+    }
+    return MAT_ERR_NONE;
+}
+
+int matrix_ew_mul(matrix *dest, matrix *src1, matrix *src2){
+    if(dest->rows != src1->rows || dest->rows != src2->cols)
+        return MAT_ERR_SIZE;
+    for(size_t row = 0; row < dest->rows; ++row){
+        for(size_t col = 0; col < dest->cols; ++col){
+            dest->data[MAT_IDX(dest, row, col)] = src1->data[MAT_IDX(src1, row, col)] * src1->data[MAT_IDX(src2, row, col)];
+        }
+    }
+    return MAT_ERR_NONE;
+}
+
+int matrix_ew_div(matrix *dest, matrix *src1, matrix *src2){
+    if(dest->rows != src1->rows || dest->rows != src2->cols)
+        return MAT_ERR_SIZE;
+    for(size_t row = 0; row < dest->rows; ++row){
+        for(size_t col = 0; col < dest->cols; ++col){
+            dest->data[MAT_IDX(dest, row, col)] = src1->data[MAT_IDX(src1, row, col)] / src1->data[MAT_IDX(src2, row, col)];
+        }
+    }
+    return MAT_ERR_NONE;
+}
+
