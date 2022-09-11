b1d870644f979eaff13927cc23e74d515fde1cd3
Marcus Behel <marcus.behel@mindspring.com>
2022-09-11 02:15:49 -0400

    Implementing matrix math in c

--- math/c_motor_math/include/matrix.h
+++ math/c_motor_math/include/matrix.h
@@ -8,8 +8,9 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 typedef struct{
-    float **data;
-    size_t dim[2];
+    float *data;
+    size_t rows;
+    size_t cols;
 } matrix;
 
 
@@ -17,7 +18,36 @@ typedef struct{
 /// Functions
 ////////////////////////////////////////////////////////////////////////////////
 
-void matrix_init(matrix *m, size_t dim0, size_t dim1);
+/**
+ * Initialize float matrix
+ * @param m Pointer to the matrix to initialize
+ * @param rows Number of rows in matrix
+ * @param cols Number of columns in matrix
+ */
+void matrix_init(matrix *m, size_t rows, size_t cols);
 
+/**
+ * Free float matrix
+ * @param m Pointer to the matrix to free
+ */
 void matrix_free(matrix *m);
 
+/**
+ * Set an item in a matrix
+ * @param m Pointer to the matrix to set an item in
+ * @param row Row to set item in
+ * @param col Column to set item in
+ * @param item Value to set
+ */
+void matrix_set_item(matrix *m, size_t row, size_t col, float item);
+
+/**
+ * Get an item in a matrix
+ * @param m Pointer to the matrix to set an item in
+ * @param row Row to set item in
+ * @param col Column to set item in
+ * @return Value of the item at the given position
+ */
+float matrix_get_item(matrix *m, size_t row, size_t col);
+
+// TODO: Set rows and columns
--- math/c_motor_math/src/matrix.c
+++ math/c_motor_math/src/matrix.c
@@ -4,15 +4,30 @@
 
 
 ////////////////////////////////////////////////////////////////////////////////
+/// Macos
+////////////////////////////////////////////////////////////////////////////////
+
+#define MATRIX_IDX(m, row, col)     (m->cols * row + col)
+
+
+////////////////////////////////////////////////////////////////////////////////
 /// Functions
 ////////////////////////////////////////////////////////////////////////////////
 
-void matrix_init(matrix *m, size_t dim0, size_t dim1){
-    m->data = calloc(dim0 * dim1, sizeof(float));
-    m->dim[0] = dim0;
-    m->dim[1] = dim1;
+void matrix_init(matrix *m, size_t rows, size_t cols){
+    m->data = calloc(rows * cols, sizeof(float));
+    m->rows = rows;
+    m->cols = cols;
 }
 
 void matrix_free(matrix *m){
     free(m->data);
+}
+
+void matrix_set_item(matrix *m, size_t row, size_t col, float item){
+    m->data[MATRIX_IDX(m, row, col)] = item;
+}
+
+float matrix_get_item(matrix *m, size_t row, size_t col){
+    return m->data[MATRIX_IDX(m, row, col)];
 }
