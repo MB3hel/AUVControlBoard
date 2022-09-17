bbecdb98c8e638c4ffa4c2098ce2bde3dad41af1
Marcus Behel <Marcus Behel>
2022-09-16 22:32:54 -0400

    Not using pio libraries for asf4, samd51a, and cmsis

---
+++ firmware/README.md
@@ -0,0 +1,21 @@
+# SW8E Control Board Firmware
+
+Firmware uses Microchip's Advanced Software Framework 4 (ASF4)
+
+## File Tree
+
+- `include`: Header files
+    - `asf4`: All Microchip ASF4 headers (config, hal, hpl, hri, etc). Extracted from Atmel Start project.
+    - `cmsis`: Common Microcontroller Software Interface Standard (CMSIS) headers for Cortex M processors Extracted from Atmel Start project.
+    - `samd51a`: SAMD51A Device Framework Package (DFP) headers. Extracted from Atmel Start package.
+    - `*.h`: Firmware headers (some extracted from Atmel Start pacakge)
+`src`: Source files
+    - `asf4`: All Microchip ASF4 sources (hal, hpl, etc). Extracted from Atmel Start project.
+    - `samd51a`: SAMD51A Device Framework Package (DFP) sources. Extracted from Atmel Start package.
+    - `*.c`: Firmware sources (some extracted from Atmel Start pacakge)
+- `ControlBoard.atstart`: Atmel Start configuration file (can be loaded in Atmel Start)
+- `import_atmel_start_proj.py`: Script to import Atmel Start project (`atzip` file). *Note: Adding more features to project may require modifications to this script. Inspect the downloaded zip file.*
+- `samd51g19a_flash_wbld.ld`: Linker script modified for ItsyBitsy M4 with UF2 bootloader
+
+
+*Note: Platformio libraries are not used for extracted cmsis, samd51a, asf4, because some source files (namely those in samd51a) must be part of executable's sources. Also just easier to keep the basic structure used by Atmel Start.*
--- firmware/import_atmel_start_proj.py
+++ firmware/import_atmel_start_proj.py
@@ -10,7 +10,7 @@ import tempfile
 
 def copyglob(glb: str, dest: str):
     if not os.path.exists(dest):
-        os.mkdir(dest)
+        os.makedirs(dest)
     for item in glob.glob(glb, recursive=True):
         if os.path.isdir(item):
             shutil.copytree(item, "{}/{}".format(dest, os.path.basename(item)))
@@ -40,41 +40,42 @@ if __name__ == "__main__":
         shutil.unpack_archive(sys.argv[1], tempdirname, format="zip")
 
         # Delete old libraries
-        shutil.rmtree("{}/lib/cmsis/include".format(script_dir), ignore_errors=True)
-        shutil.rmtree("{}/lib/samd51a/include".format(script_dir), ignore_errors=True)
-        shutil.rmtree("{}/lib/samd51a/src".format(script_dir), ignore_errors=True)
-        shutil.rmtree("{}/lib/asf4/include".format(script_dir), ignore_errors=True)
-        shutil.rmtree("{}/lib/asf4/src".format(script_dir), ignore_errors=True)
+        shutil.rmtree("{}/include/cmsis".format(script_dir), ignore_errors=True)
+        shutil.rmtree("{}/include/samd51a".format(script_dir), ignore_errors=True)
+        shutil.rmtree("{}/src/samd51a".format(script_dir), ignore_errors=True)
+        shutil.rmtree("{}/include/asf4".format(script_dir), ignore_errors=True)
+        shutil.rmtree("{}/src/asf4".format(script_dir), ignore_errors=True)
 
         # Copy CMSIS library
         print("Copying CMSIS...")
-        copyglob("{}/CMSIS/Core/Include/*.h".format(tempdirname), "{}/lib/cmsis/include".format(script_dir))
+        copyglob("{}/CMSIS/Core/Include/*.h".format(tempdirname), "{}/include/cmsis/".format(script_dir))
         
         # Copy samd51a DFP
         print("Copying SAMD51A_DFP...")
-        copyglob("{}/samd51a/include/*".format(tempdirname), "{}/lib/samd51a/include".format(script_dir))
-        copyglob("{}/samd51a/gcc/*.c".format(tempdirname), "{}/src".format(script_dir))
+        copyglob("{}/samd51a/include/*".format(tempdirname), "{}/include/samd51a/".format(script_dir))
+        copyglob("{}/samd51a/gcc/*.c".format(tempdirname), "{}/src/samd51a".format(script_dir))
+        copyglob("{}/samd51a/gcc/gcc/*.c".format(tempdirname), "{}/src/samd51a".format(script_dir))
 
         # Copy ASF4 hal Libraries
         print("Copying ASF4 hal...")
-        copyglob("{}/hal/include/*.h".format(tempdirname), "{}/lib/asf4/include".format(script_dir))
-        copyglob("{}/hal/src/*.c".format(tempdirname), "{}/lib/asf4/src".format(script_dir))
-        copyglob("{}/hal/utils/include/*.h".format(tempdirname), "{}/lib/asf4/include".format(script_dir))
-        copyglob("{}/hal/utils/src/*.c".format(tempdirname), "{}/lib/asf4/src".format(script_dir))
+        copyglob("{}/hal/include/*.h".format(tempdirname), "{}/include/asf4/hal".format(script_dir))
+        copyglob("{}/hal/src/*.c".format(tempdirname), "{}/src/asf4/hal".format(script_dir))
+        copyglob("{}/hal/utils/include/*.h".format(tempdirname), "{}/include/asf4/hal/utils".format(script_dir))
+        copyglob("{}/hal/utils/src/*.c".format(tempdirname), "{}/src/asf4/hal/utils".format(script_dir))
 
         # Copy ASF4 hpl Libraries
         print("Copying ASF4 hpl...")
-        copyglob("{}/hpl/**/*.h".format(tempdirname), "{}/lib/asf4/include".format(script_dir))
-        copyglob("{}/hpl/**/*.c".format(tempdirname), "{}/lib/asf4/src".format(script_dir))
+        copyglob("{}/hpl/**/*.h".format(tempdirname), "{}/include/asf4/hpl".format(script_dir))
+        copyglob("{}/hpl/**/*.c".format(tempdirname), "{}/src/asf4/hpl".format(script_dir))
 
         # Copy ASF4 hri
         print("Copying ASF4 hri...")
-        copyglob("{}/hri/*.h".format(tempdirname), "{}/lib/asf4/include".format(script_dir))
+        copyglob("{}/hri/*.h".format(tempdirname), "{}/include/asf4/hri".format(script_dir))
 
 
         # Copy ASF4 config
         print("Copying ASF4 config...")
-        copyglob("{}/config/*.h".format(tempdirname), "{}/lib/asf4/include".format(script_dir))
+        copyglob("{}/config/*.h".format(tempdirname), "{}/include/asf4/config".format(script_dir))
 
         # Copy src files
         print("Copying ASF4 init files...")
@@ -87,8 +88,8 @@ if __name__ == "__main__":
         # Copy usb files
         if os.path.exists("{}/usb".format(tempdirname)):
             print("Copying ASF4 usb files...")
-            copyglob("{}/usb/**/*.h".format(tempdirname), "{}/lib/asf4/include".format(script_dir))
-            copyglob("{}/usb/**/*.c".format(tempdirname), "{}/lib/asf4/src".format(script_dir))
+            copyglob("{}/usb/**/*.h".format(tempdirname), "{}/include/asf4/usb".format(script_dir))
+            copyglob("{}/usb/**/*.c".format(tempdirname), "{}/src/asf4/usb".format(script_dir))
             copyreplacefile("{}/usb_start.c".format(tempdirname), "{}/src/usb_start.c".format(script_dir))
             copyreplacefile("{}/usb_start.h".format(tempdirname), "{}/include/usb_start.h".format(script_dir))
 
