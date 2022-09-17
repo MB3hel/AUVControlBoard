67d482bf5650853cd525b191e08036ecaad2b3f4
Marcus Behel <marcus.behel@mindspring.com>
2022-09-17 15:14:26 -0400

    Added comments on how usb stuff works

--- firmware/src/usbuart.c
+++ firmware/src/usbuart.c
@@ -66,27 +66,33 @@ bool usbuart_state_callback(usb_cdc_control_signal_t state){
         cdcdf_acm_register_callback(CDCDF_ACM_CB_READ, (FUNC_PTR)usbuart_read_callback);
         cdcdf_acm_register_callback(CDCDF_ACM_CB_WRITE, (FUNC_PTR)usbuart_write_callback);
 
-        // Initialize TX
+        // Initial write
+        // Nothing is actually written, but the write callback is invoked when done
+        // So next write should occur in the write callback
         cdcdf_acm_write((uint8_t *)usbuart_tx_buf, 0);
 
-        // Initialize RX
+        // Initial read
+        // Any data read will be available in the read callback
+        // The next read should occur in the read callback
         cdcdf_acm_read((uint8_t *)usbuart_rx_buf, USBUART_RX_BUF_SIZE);
     }
     return false;
 }
 
+// Called when read completes. "count" is number of bytes that were read
 bool usbuart_read_callback(const uint8_t ep, const enum usb_xfer_code rc, const uint32_t count){
-    // TODO: Additional read calls here
+    // TODO: Move previously read  data into read buffer
+    // TODO: Start next read by calling cdcdf_acm_read()
 
     // No error
     return false;
 }
 
+// Called when write complets. "count" is number of bytes that were written
 bool usbuart_write_callback(const uint8_t ep, const enum usb_xfer_code rc, const uint32_t count){
-    // TODO: Additional write calls here
-
-    uint8_t *data = (uint8_t[]){'H', 'E', 'L', 'L', 'O', '\r', '\n'};
-    cdcdf_acm_write(data, 7);
+    // TODO: Remove previously writen data from the tx buffer
+    // TODO: Start next write (however many bytes in tx buffer up to limit for usb cdc)
+    //       Write 0 bytes if nothing in tx buffer just so this funciton is called again
 
     // No error
     return false;
