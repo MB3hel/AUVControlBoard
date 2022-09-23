f973d2706b6571ca5a15919da9404e8149b24440
Marcus Behel <marcus.behel@mindspring.com>
2022-09-22 20:50:50 -0400

    Fixed dotstar setup

--- firmware/src/dotstar.c
+++ firmware/src/dotstar.c
@@ -12,9 +12,8 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void dotstar_write_byte(uint8_t val){
-    for (uint8_t bit = 0u; bit < 8u; bit++){
-        gpio_set_pin_level(DS_DAT, (val & (1 << bit)) ? true : false);
-        delay_us(1);
+    for (uint8_t bit = 8; bit > 0; bit--){
+        gpio_set_pin_level(DS_DAT, (val & (1 << (bit - 1))) ? true : false);
         gpio_set_pin_level(DS_CLK, true);
         delay_us(1);
         gpio_set_pin_level(DS_CLK, false);
@@ -23,15 +22,19 @@ void dotstar_write_byte(uint8_t val){
 }
 
 void dotstar_init(void){
-    dotstar_set(0, 0, 0);
+    dotstar_set(10, 10, 0);
     gpio_set_pin_level(DS_DAT, false);
-    gpio_set_pin_level(DS_CLK, true);
+    gpio_set_pin_level(DS_CLK, false);
 }
 
 void dotstar_set(uint8_t r, uint8_t g, uint8_t b){
+    dotstar_write_byte(0x00);
+    dotstar_write_byte(0x00);
+    dotstar_write_byte(0x00);
+    dotstar_write_byte(0x00);
     dotstar_write_byte(0xFF);
-    dotstar_write_byte(r);
-    dotstar_write_byte(g);
     dotstar_write_byte(b);
+    dotstar_write_byte(g);
+    dotstar_write_byte(r);
     dotstar_write_byte(0xFF);
 }
