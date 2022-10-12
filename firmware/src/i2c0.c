1fb8448271751fad58d97ce27f08036f73129e7c
Marcus Behel <marcus.behel@mindspring.com>
2022-10-12 19:23:56 -0400

    using i2c without hal io layer

--- firmware/src/i2c0.c
+++ firmware/src/i2c0.c
@@ -25,7 +25,6 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 static uint8_t curr_state, next_state;                          // Current state and next state
-static struct io_descriptor *io;                                // Io descriptor
 
 // Queue of messages (implemented as circular buffer)
 // widx is index to insert next item (write)
@@ -69,7 +68,6 @@ void i2c0_init(void){
     trans_queue_ridx = 0;
     trans_queue_count = 0;
 
-    i2c_m_async_get_io_descriptor(&I2C, &io);                   // Store for later
     i2c_m_async_enable(&I2C);                                   // Enable bus (ASF layer)
     i2c_m_async_register_callback(&I2C, 
             I2C_M_ASYNC_TX_COMPLETE, 
@@ -87,12 +85,11 @@ void i2c0_init(void){
 }
 
 void i2c0_process(void){
+    struct _i2c_m_msg msg;
+
     if(curr_state == next_state)
         // No state change, so no actions necessary
         return;
-
-    bool write_finished_now = false;
-    bool read_finished_now = false;
 
     // Transition before performing actions because callbacks can interrupt
     // and modify next_state when io_write or io_read complete
@@ -105,20 +102,22 @@ void i2c0_process(void){
         // Start the current transaction's write phase
         // Callback transitions to read state when write finishes
         i2c_m_async_set_slaveaddr(&I2C, trans_queue[trans_queue_ridx]->address, I2C_M_SEVEN);
-        if(trans_queue[trans_queue_ridx]->write_count > 0)
-            io_write(io, trans_queue[trans_queue_ridx]->write_buf, trans_queue[trans_queue_ridx]->write_count);
-        else
-            write_finished_now = true;
+        msg.addr = trans_queue[trans_queue_ridx]->address;
+        msg.buffer = trans_queue[trans_queue_ridx]->write_buf;
+	    msg.len = trans_queue[trans_queue_ridx]->write_count;
+        msg.flags = 0;
+        i2c_m_async_transfer(&I2C, &msg);
         break;
     case STATE_READ:
         // Switching to read state after write finished
         // ridx points to current transaction
         // Start the current transaction's read phase
         // Callback transitions to idle state when read finishes
-        if(trans_queue[trans_queue_ridx]->read_count > 0)
-            io_read(io, trans_queue[trans_queue_ridx]->read_buf, trans_queue[trans_queue_ridx]->read_count);
-        else
-            read_finished_now = true;
+        msg.addr = trans_queue[trans_queue_ridx]->address;
+        msg.buffer = trans_queue[trans_queue_ridx]->read_buf;
+	    msg.len = trans_queue[trans_queue_ridx]->read_count;
+        msg.flags = I2C_M_RD | I2C_M_STOP;
+        i2c_m_async_transfer(&I2C, &msg);
         break;
     case STATE_IDLE:
         // Switching to idle state after read finished
@@ -141,15 +140,6 @@ void i2c0_process(void){
             next_state = STATE_WRITE;
             FLAG_SET(flags_main, FLAG_MAIN_I2C0_PROC);
         }
-    }
-
-    // If a read or write state just started, but zero bytes consider it finished now
-    // Callback will never happen automatically, so call it manually to trigger state transition
-    if(write_finished_now){
-        cb_tx_complete(&I2C);
-    }
-    if(read_finished_now){
-        cb_rx_complete(&I2C);
     }
 }
 
