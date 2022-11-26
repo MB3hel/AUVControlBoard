b7868ad6f3d2637c504f905a26335baf2ef92f81
Marcus Behel <marcus.behel@mindspring.com>
2022-11-25 21:46:00 -0500

    Fix v2 generator proj gitignore

---
+++ firmware/generator_projects/ControlBoard_v2/supplemental/libc_syscalls.c
@@ -0,0 +1,16 @@
+
+int _close(int file) {
+	return -1;
+}
+
+int _lseek(int file, int ptr, int dir) {
+	return 0;
+}
+
+int _write (int file, char * ptr, int len) {
+	return -1;
+}
+
+int _read (int file, char * ptr, int len) {
+	return -1;
+}
