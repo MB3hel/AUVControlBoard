732a21ac1ac3da38740065b2c7664c23fdb3321d
Marcus Behel <marcus.behel@mindspring.com>
2022-12-22 17:42:43 -0500

    Increase timer task stack size

--- firmware/include/v1/FreeRTOSConfig.h
+++ firmware/include/v1/FreeRTOSConfig.h
@@ -74,7 +74,7 @@ extern void vAssertCalled(const char *file, unsigned int line);
 #define configUSE_TIMERS                        1
 #define configTIMER_TASK_PRIORITY               2
 #define configTIMER_QUEUE_LENGTH                4
-#define configTIMER_TASK_STACK_DEPTH            configMINIMAL_STACK_SIZE
+#define configTIMER_TASK_STACK_DEPTH            (6*configMINIMAL_STACK_SIZE)
 
 /* Interrupt nesting behaviour configuration. */
 #define configPRIO_BITS                         		3
