770d20ff5d7b275db3c2d7b611cdd696aae879a0
Marcus Behel <marcus.behel@mindspring.com>
2023-01-11 14:39:21 -0500

    Add local and global mode test scripts

---
+++ iface/localmode.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+
+from control_board import ControlBoard
+import sys
+import time
+from serial import SerialException
+from typing import Callable
+
+
+def main() -> int:
+    ############################################################################
+    # Open communication with control board
+    ############################################################################
+    port = "/dev/ttyACM0"
+    if len(sys.argv) > 1:
+        port = sys.argv[1]
+    cb = ControlBoard(port)
+
+    ############################################################################
+    # Setup
+    ############################################################################
+    print("Set motor matrix...", end="")
+    mat = ControlBoard.MotorMatrix()
+    #        MotorNum    x      y      z    pitch   roll     yaw
+    mat.set_row(3,    [ -1,    -1,     0,     0,      0,     +1   ])
+    mat.set_row(4,    [ +1,    -1,     0,     0,      0,     -1   ])
+    mat.set_row(1,    [ -1,    +1,     0,     0,      0,     -1   ])
+    mat.set_row(2,    [ +1,    +1,     0,     0,      0,     +1   ])
+    mat.set_row(7,    [  0,     0,    -1,    -1,     -1,      0   ])
+    mat.set_row(8,    [  0,     0,    -1,    -1,     +1,      0   ])
+    mat.set_row(5,    [  0,     0,    -1,    +1,     -1,      0   ])
+    mat.set_row(6,    [  0,     0,    -1,    +1,     +1,      0   ])
+    if cb.set_motor_matrix(mat) == ControlBoard.AckError.NONE:
+        print("Done.")
+    else:
+        print("Fail.")
+        return 1
+
+    print("Set thruster inversions...", end="")
+    if cb.set_tinv([True, True, False, False, True, False, False, True]) == ControlBoard.AckError.NONE:
+        print("Done.")
+    else:
+        print("Fail.")
+        return 1
+    
+    ############################################################################
+    # Local mode set
+    ############################################################################
+
+    print("Setting local mode vector...")
+    #                x    y    z    p    r    y
+    if cb.set_local(0.0, 0.3, 0.0, 0.0, 0.0, 0.0) == ControlBoard.AckError.NONE:
+        print("Done.")
+    else:
+        print("Fail.")
+        return 1
+    
+    print("Press enter to stop...")
+
+    print("Stopping...")
+    if cb.set_local(0.0, 0.0, 0.0, 0.0, 0.0, 0.0) == ControlBoard.AckError.NONE:
+        print("Done.")
+    else:
+        print("Fail.")
+        return 1
+
+
+    return 0
+
+if __name__ == "__main__":
+    try:
+        sys.exit(main())
+    except KeyboardInterrupt:
+        exit(0)
+    except SerialException:
+        print("Serial communication failure!")
+        exit(2)
