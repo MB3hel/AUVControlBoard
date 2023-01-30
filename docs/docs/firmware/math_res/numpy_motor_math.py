423c20c6324b9c69b634994f5779692bc234d7c3
Marcus Behel <marcus.behel@mindspring.com>
2023-01-30 01:54:03 -0500

    Changed global math in numpy script to use quaternion math

--- docs/docs/firmware/math_res/numpy_motor_math.py
+++ docs/docs/firmware/math_res/numpy_motor_math.py
@@ -3,6 +3,106 @@
 import numpy as np
 
 
+
+# ZYX (deg)
+class Euler:
+    def __init__(self, pitch: np.float32 = 0.0, roll: np.float32 = 0.0, yaw: np.float32 = 0.0):
+        # In degrees
+        self.pitch: np.float32 = pitch
+        self.roll: np.float32 = roll
+        self.yaw: np.float32 = yaw
+    
+    def __repr__(self) -> str:
+        return "Euler(pitch = {}, roll = {}, yaw = {})".format(self.pitch, self.roll, self.yaw)
+    
+    def to_quaternion(self) -> 'Quaternion':
+        ## Pitch about X, Roll about Y, Yaw about Z
+
+        roll_rad = np.deg2rad(self.roll)
+        pitch_rad = np.deg2rad(self.pitch)
+        yaw_rad = np.deg2rad(self.yaw)
+
+        cr = np.cos(roll_rad * 0.5)
+        sr = np.sin(roll_rad * 0.5)
+        cp = np.cos(pitch_rad * 0.5)
+        sp = np.sin(pitch_rad * 0.5)
+        cy = np.cos(yaw_rad * 0.5)
+        sy = np.sin(yaw_rad * 0.5)
+
+        q = Quaternion()
+        q.w = cp * cr * cy + sp * sr * sy
+        q.x = sp * cr * cy - cp * sr * sy
+        q.y = cp * sr * cy + sp * cr * sy
+        q.z = cp * cr * sy - sp * sr * cy
+
+        return q
+
+
+class Quaternion:
+    def __init__(self, w: np.float32 = 0.0, x: np.float32 = 0.0, y: np.float32 = 0.0, z: np.float32 = 0.0):
+        self.w: np.float32 = w
+        self.x: np.float32 = x
+        self.y: np.float32 = y
+        self.z: np.float32 = z
+    
+    def __repr__(self) -> str:
+        return "Quaternion(w = {}, x = {}, y = {}, z = {})".format(self.w, self.x, self.y, self.z)
+    
+    def to_euler(self) -> Euler:
+        angles = Euler()
+
+        # Pitch (about x axis)
+        t0 = +2.0 * (self.w * self.x + self.y * self.z)
+        t1 = +1.0 - 2.0 * (self.x * self.x + self.y * self.y)
+        angles.pitch = np.arctan2(t0, t1)
+     
+        # Roll (about y axis)
+        t2 = +2.0 * (self.w * self.y - self.z * self.x)
+        t2 = +1.0 if t2 > +1.0 else t2
+        t2 = -1.0 if t2 < -1.0 else t2
+        angles.roll = np.arcsin(t2)
+     
+        # Yaw (about z axis)
+        t3 = +2.0 * (self.w * self.z + self.x * self.y)
+        t4 = +1.0 - 2.0 * (self.y * self.y + self.z * self.z)
+        angles.yaw = np.arctan2(t3, t4)
+     
+        # Convert to degrees
+        angles.pitch = np.rad2deg(angles.pitch)
+        angles.roll = np.rad2deg(angles.roll)
+        angles.yaw = np.rad2deg(angles.yaw)
+
+        return angles
+    
+    def multiply_scalar(self, s: np.float32) -> 'Quaternion':
+        return Quaternion(self.w * s, self.x * s, self.y * s, self.z * s)
+
+    def divide_scalar(self, s: np.float32) -> 'Quaternion':
+        return Quaternion(self.w / s, self.x / s, self.y / s, self.z / s)
+
+    def multiply(self, second: 'Quaternion') -> 'Quaternion':
+        res = Quaternion()
+        res.w = self.w * second.w - self.x * second.x - self.y * second.y - self.z * second.z
+        res.x = self.w * second.x + self.x * second.w + self.y * second.z - self.z * second.y
+        res.y = self.w * second.y - self.x * second.z + self.y * second.w + self.z * second.x
+        res.z = self.w * second.z + self.x * second.y - self.y * second.x + self.z * second.w
+        return res
+
+    def inverse(self) -> 'Quaternion':
+        return self.conjugate().divide_scalar(self.magnitude())
+    
+    def conjugate(self) -> 'Quaternion':
+        return Quaternion(self.w, -self.x, -self.y, -self.z)
+
+    def magnitude(self) -> np.float32:
+        return np.sqrt(self.w*self.w + self.x*self.x + self.y*self.y + self.z*self.z)
+    
+    def dot(self, second: 'Quaternion') -> np.float32:
+        return self.w * second.w + self.x * second.x + self.y * second.y + self.z * second.z
+
+
+
+
 ################################################################################
 # Thruster Configuration Information
 ################################################################################
@@ -37,9 +137,10 @@ for r in range(np.size(contribution_matrix, axis=0)):
 ################################################################################
 
 # Current orientation in degrees (used in global mode)
-curr_pitch = np.float32(90.0)
-curr_roll = np.float32(0.0)
-curr_yaw = np.float32(0.0)
+curr_euler = Euler(90.0, 0.0, 0.0)
+
+# BNO055 gives quaternion, so implement the math here same way
+curr_quat = curr_euler.to_quaternion()
 
 
 ################################################################################
@@ -59,21 +160,17 @@ target_is_global = True
 ################################################################################
 
 if target_is_global:
-    cp = np.cos(np.deg2rad(curr_pitch))
-    sp = np.sin(np.deg2rad(curr_pitch))
-    cr = np.cos(np.deg2rad(curr_roll))
-    sr = np.sin(np.deg2rad(curr_roll))
-    R_x = np.matrix([
-        [   1,      0,      0       ],
-        [   0,      cp,     -sp     ],
-        [   0,      sp,     cp      ]
+    w = curr_quat.w
+    x = curr_quat.x
+    y = curr_quat.y
+    z = curr_quat.z
+    n = w*w + x*x + y*y + z*z
+    s = np.float32(0) if n == 0 else np.float32(2) / n
+    R = np.matrix([
+        [1 - s*(y*y + z*z),         s*(x*y - w*z),          s*(x*z + w*y)],
+        [s*(x*y + w*z),             1 - s*(x*x + z*z),      s*(y*z - w*x)],
+        [s*(x*z - w*y),             s*(y*z + w*x),          1 - s*(x*x + y*y)]
     ], dtype=np.float32)
-    R_y = np.matrix([
-        [   cr,     0,      sr      ],
-        [   0 ,     1,      0       ],
-        [   -sr,    0,      cr      ]
-    ], dtype=np.float32)
-    R = np.matmul(R_y, R_x)
     target_1d = target.flatten().A1
     translation = target_1d[0:3].reshape(3, 1)
     rotation = target_1d[3:6].reshape(3, 1)
