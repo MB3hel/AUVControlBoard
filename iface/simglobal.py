86b297f0e4ee18a5ef14d2537eb495ef2e308727
Marcus Behel <marcus.behel@mindspring.com>
2023-02-10 23:53:22 -0500

    Better sim test

--- iface/simglobal.py
+++ iface/simglobal.py
@@ -1,11 +1,24 @@
 
 from control_board import ControlBoard, Simulator
 import time
+import threading
 
 
-s = Simulator()
+s = Simulator(cb_debug=True)
 cb = s.control_board
 s.reset_sim()
 s.set_robot_rot(*s.euler_to_quat(90, 0, 0))
 time.sleep(1)
 res = cb.set_global(0.0, 0.0, 0.3, 0.2, 0.0, 0.0)
+t_stop = False
+def do_feed():
+    while not t_stop:
+        cb.feed_motor_watchdog()
+        time.sleep(0.25)
+t = threading.Thread(daemon=True, target=do_feed)
+t.start()
+print("Press enter to stop...", end="")
+input("")
+t_stop = True
+t.join()
+cb.set_global(0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
