027d1f953a01fd812e42a129b37b469acc7ab9c3
Marcus Behel <marcus.behel@mindspring.com>
2023-02-19 20:19:18 -0500

    Fix unhandled pwm set failure on v1

--- firmware/src/v1/thruster.c
+++ firmware/src/v1/thruster.c
@@ -21,6 +21,9 @@
 #include <debug.h>
 #include <string.h>
 #include <stdlib.h>
+#include <led.h>
+#include <FreeRTOS.h>
+#include <task.h>
 
 void thruster_init(void){
     float *zero_speeds = (float[]){0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f};
@@ -42,26 +45,26 @@ void thruster_set(float *speeds){
     #define PULSE_WIDTH_US(speed)       ((int)(400 * speed) + 1500)
     
     // THR1 = TCC0[2]
-    TCC0_PWM24bitDutySet(TCC0_CHANNEL2, PULSE_WIDTH_US(speeds[0]) * COUNT_PER_US);
+    while(!TCC0_PWM24bitDutySet(TCC0_CHANNEL2, PULSE_WIDTH_US(speeds[0]) * COUNT_PER_US));
 
     // THR2 = TCC0[3]
-    TCC0_PWM24bitDutySet(TCC0_CHANNEL3, PULSE_WIDTH_US(speeds[1]) * COUNT_PER_US);
+    while(!TCC0_PWM24bitDutySet(TCC0_CHANNEL3, PULSE_WIDTH_US(speeds[1]) * COUNT_PER_US));
 
     // THR3 = TCC0[1]
-    TCC0_PWM24bitDutySet(TCC0_CHANNEL1, PULSE_WIDTH_US(speeds[2]) * COUNT_PER_US);
+    while(!TCC0_PWM24bitDutySet(TCC0_CHANNEL1, PULSE_WIDTH_US(speeds[2]) * COUNT_PER_US));
 
     // THR4 = TCC0[0]
-    TCC0_PWM24bitDutySet(TCC0_CHANNEL0, PULSE_WIDTH_US(speeds[3]) * COUNT_PER_US);
+    while(!TCC0_PWM24bitDutySet(TCC0_CHANNEL0, PULSE_WIDTH_US(speeds[3]) * COUNT_PER_US));
 
     // THR5 = TCC1[3]
-    TCC1_PWM24bitDutySet(TCC1_CHANNEL3, PULSE_WIDTH_US(speeds[4]) * COUNT_PER_US);
+    while(!TCC1_PWM24bitDutySet(TCC1_CHANNEL3, PULSE_WIDTH_US(speeds[4]) * COUNT_PER_US));
 
     // THR6 = TCC1[2]
-    TCC1_PWM24bitDutySet(TCC1_CHANNEL2, PULSE_WIDTH_US(speeds[5]) * COUNT_PER_US);
+    while(!TCC1_PWM24bitDutySet(TCC1_CHANNEL2, PULSE_WIDTH_US(speeds[5]) * COUNT_PER_US));
 
     // THR7 = TCC1[1]
-    TCC1_PWM24bitDutySet(TCC1_CHANNEL1, PULSE_WIDTH_US(speeds[6]) * COUNT_PER_US);
+    while(!TCC1_PWM24bitDutySet(TCC1_CHANNEL1, PULSE_WIDTH_US(speeds[6]) * COUNT_PER_US));
 
     // THR8 = TCC1[0]
-    TCC1_PWM24bitDutySet(TCC1_CHANNEL0, PULSE_WIDTH_US(speeds[7]) * COUNT_PER_US);
+    while(!TCC1_PWM24bitDutySet(TCC1_CHANNEL0, PULSE_WIDTH_US(speeds[7]) * COUNT_PER_US));
 }
