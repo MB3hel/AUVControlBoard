2e44ed1dee8936c134ccf7f9139d0475b7038465
Marcus Behel <marcus.behel@mindspring.com>
2023-11-02 00:51:13 -0400

    Start working on packaging restructure for simcb

--- iface/control_board.py
+++ iface/control_board.py
@@ -37,7 +37,7 @@ from typing import List, Dict, Tuple
 
 # Version of interface scripts (automatically updated by package.sh)
 # DO NOT MODIFY MANUALLY!!!
-VER_STR = "1.0.3"
+VER_STR = "1.1.0-b0"
 
 
 START_BYTE = b'\xfd'
