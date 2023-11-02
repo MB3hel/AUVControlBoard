5c1745b328c12c872271dd2d2fa19832b7634a6b
Marcus Behel <marcus.behel@mindspring.com>
2023-11-02 19:54:29 -0400

    Implemented packaging for simcb linux

---
+++ package/pack_simcb_linux.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+#!/usr/bin/env bash
+
+# Exit if any command errors
+set -e
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+trap 'echo "\"${last_command}\" command filed with exit code $?."' ERR
+
+# Work in same directory as this script
+DIR=$(realpath $(dirname "$0"))
+cd "$DIR"
+
+# Base directory structure
+rm -rf pack/simcb/linux-amd64/
+if [ ! -d pack/simcb ]; then
+    mkdir pack/simcb/
+fi
+mkdir pack/simcb/linux-amd64/
+
+# Build firmware
+pushd ../firmware > /dev/null
+rm -rf build/
+cmake --preset simcb-linux
+cmake --build --preset simcb-linux-release
+popd
+
+# Copy firmware files
+cp -r ../firmware/build/simcb-linux/Release/SimCB pack/simcb/linux-amd64/
+cp ./simcb-linux-launch.sh pack/simcb/linux-amd64/launch.sh
+chmod +x pack/simcb/linux-amd64/launch.sh
