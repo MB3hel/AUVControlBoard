b80434adbc7b015bf7558211d98e5eaef460189c
Marcus Behel <marcus.behel@mindspring.com>
2023-11-14 23:14:42 -0500

    Update version

--- iface/control_board.py
+++ iface/control_board.py
@@ -37,7 +37,7 @@ from typing import List, Dict, Tuple
 
 # Version of interface scripts (automatically updated by package.sh)
 # DO NOT MODIFY MANUALLY!!!
-VER_STR = "1.1.0-b0"
+VER_STR = "1.1.0-b1"
 
 
 START_BYTE = b'\xfd'
